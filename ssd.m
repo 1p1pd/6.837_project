aeaa04cea16a1e43f15785a0b4a0a5b617c2b064
Yifan Wang <james@dhcp-18-111-51-126.dyn.mit.edu>
2016-12-09 15:31:11 -0500

    ssd finished

---
+++ ssd.m
@@ -0,0 +1,85 @@
+clear all
+clc
+theta = 30;
+theta = (450 - theta) * pi / 180;
+joint0 = [0, 5];
+joint1 = [0, 0];
+joint2 = [5 * cos(theta), 5 * sin(theta)];
+bone0 = zeros(3, 101);
+bone1 = zeros(3, 101);
+skin0_left = zeros(3, 101);
+skin0_right = zeros(3, 101);
+skin1_left = zeros(3, 101);
+skin1_right = zeros(3, 101);
+bind_left = zeros(3, 201);
+bind_right = zeros(3, 201);
+index = 1;
+while index < 102
+    bone0(:,index) = [0, 5 * (101 - index) / 100, 1];
+    skin0_left(:,index) = [-0.5, 5 * (101 - index) / 100, 1];
+    skin0_right(:,index) = [0.5, 5 * (101 - index) / 100, 1];
+    index = index + 1;
+end
+index = 1;
+while index < 202
+    bind_left(:,index) = [-0.5, 5 * (101 - index) / 100, 1];
+    bind_right(:,index) = [0.5, 5 * (101 - index) / 100, 1];
+    index = index + 1;
+end
+phi = theta - 1.5 * pi;
+trans = [cos(phi), -sin(phi), 0; sin(phi), cos(phi), 0; 0, 0, 1] * [1, 0, 0; 0, 1, -5; 0, 0, 1];
+index = 1;
+while index < 102
+    bone1(:,index) = trans * bone0(:,index);
+    skin1_left(:,index) = trans * skin0_left(:,index);
+    skin1_right(:,index) = trans * skin0_right(:,index);
+    index = index + 1;
+end
+figure(1)
+plot(bone0(1, :),bone0(2, :))
+hold on
+plot(bone1(1, :),bone1(2, :))
+hold on
+xlim([-8 8]);
+ylim([-6 6]);
+index = 1;
+% plot(skin0_left(1, :),skin0_left(2, :))
+% hold on
+% plot(skin0_right(1, :),skin0_right(2, :))
+% hold on
+% plot(skin1_left(1, :),skin1_left(2, :))
+% hold on
+% plot(skin1_right(1, :),skin1_right(2, :))
+% hold on
+weights = zeros(2, 201);
+index = 1;
+fraction = 80;
+while index < 202
+    if index <= 101
+        if index <= fraction
+            weights(:,index) = [1; 0];
+        else
+            temp = 0.5 * (index - fraction) / (101 - fraction);
+            weights(:,index) = [1 - temp; temp];
+        end
+    else
+        weights(:,index) = [1; 1] - weights(:,202 - index);
+    end
+    index = index + 1;
+end
+ssd_left = zeros(3, 201);
+ssd_right = zeros(3, 201);
+trans = [cos(phi), -sin(phi), 0; sin(phi), cos(phi), 0; 0, 0, 1];
+index = 1;
+while index < 202
+    w = weights(:, index);
+    s_t = w(1) * eye(3) + w(2) * trans;
+    ssd_left(:,index) = s_t * bind_left(:, index);
+    ssd_right(:,index) = s_t * bind_right(:, index);
+    index = index + 1;
+end
+plot(ssd_left(1, :), ssd_left(2, :))
+hold on
+plot(ssd_right(1, :), ssd_right(2, :))
+hold on
+
